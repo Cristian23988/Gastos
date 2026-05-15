048cb54fd423f8918d056190cfe087be5dfc2b51
cristian23988 <cfbravo@unimayor.edu.co>
2026-05-14 19:05:48 -0500

    init

--- app.py
+++ app.py
@@ -118,7 +118,8 @@ def guardar_salario():
 
     return redirect("/")
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
